fe72e445074af3b827c43b5e257a8d23f4eff77c
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-11-04 19:25:52 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.py
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.py
@@ -1,9 +1,12 @@
 # 15489. 파스칼 삼각형
 
+# 입력 받기
+# r번째 줄, c번째 수를 위 꼭짓점, 한 변의 길이 w
 r, c, w = map(int, input().split())
 
-pt = [[1 for _ in range(i)] for i in range(1, r + w)]
+pt = [[1] * (r + w) for _ in range(r + w)]
 
+# 파스칼 삼각형 만들기
 for i in range(1, r + w - 1):
     for j in range(1, i):
         pt[i][j] = pt[i - 1][j - 1] + pt[i - 1][j]
