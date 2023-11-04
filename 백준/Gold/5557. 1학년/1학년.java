3c53271ca2915c90b0cb437ae6cc89494ae24521
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-11-04 21:21:49 +0900

    [Gold V] Title: 1학년, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -3,39 +3,47 @@ import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" "); 
+        st = new StringTokenizer(br.readLine());
         int[] nums = new int[n];
-
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(input[i]);
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-
         int target = nums[n - 1];
+        long[] dp = new long[21];
 
-        // dp[i][j] := i번째 숫자까지 사용해서 j 값을 만들 수 있는 경우의 수
-        long[][] dp = new long[n][21];
-        dp[0][nums[0]] = 1L;
+        // 첫 번째 숫자로 시작
+        dp[nums[0]] = 1;
 
+        // 다이나믹 프로그래밍으로 dp 테이블 채우기
         for (int i = 1; i < n - 1; i++) {
+            long[] next = new long[21];
             for (int j = 0; j <= 20; j++) {
-                if (j + nums[i] <= 20) {
-                    dp[i][j] += dp[i - 1][j + nums[i]];
-                }
-                if (j - nums[i] >= 0) {
-                    dp[i][j] += dp[i - 1][j - nums[i]];
+                if (dp[j] != 0) {
+                    if (j + nums[i] <= 20) {
+                        next[j + nums[i]] += dp[j];
+                    }
+                    if (j - nums[i] >= 0) {
+                        next[j - nums[i]] += dp[j];
+                    }
                 }
             }
+            dp = next;
         }
 
-        bw.write(dp[n - 2][target] + "\n");
+        // 결과 출력
+        bw.write(String.valueOf(dp[target]));
+        bw.newLine();
 
+        // 리소스 해제
         br.close();
         bw.flush();
         bw.close();
